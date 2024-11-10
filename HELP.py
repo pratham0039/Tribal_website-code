c1b716b7650d72f38f48acedc61e2494e415eab4
pratham0039 <2021uee0150@iitjammu.ac.in>
2024-11-10 12:15:08 +0530

    commit poonch photos

--- HELP.py
+++ HELP.py
@@ -1,7 +1,7 @@
 import os
 
 # Specify the folder path
-folder_path = 'images/rajouri'  # Replace with your folder path
+folder_path = 'images/poonch'  # Replace with your folder path
 
 # List of supported image file extensions
 image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp')
@@ -21,12 +21,12 @@ with open('image_list.html', 'w') as file:
     # Write the list of image filenames as HTML
     for image in image_files:
         file.write(f'''
-  <li class="col-xs-6 col-sm-4 col-md-3" data-responsive='images/rajouri/{image}' data-src='images/rajouri/{image}'
-                        data-sub-html='<b>Place: </b>Rajouri
+  <li class="col-xs-6 col-sm-4 col-md-3" data-responsive='images/poonch/{image}' data-src='images/poonch/{image}'
+                        data-sub-html='<b>Place: </b>poonch
 '><a href="">
-                            <img class="img-responsive img-thumbnail" style="padding: 1px" src='images/rajouri/{image}' height="150px" width="360px" />
+                            <img class="img-responsive img-thumbnail" style="padding: 1px" src='images/poonch/{image}' height="150px" width="360px" />
                            </a>
-                        <h5 class="img-responsive" style="display: ruby-text-group; font-size: .70em; width: 80%; height: 15px; margin-top: 0.85em; margin-bottom: .67em; color: white; margin-left: 0; margin-right: 0;">Rajouri</h5>
+                        <h5 class="img-responsive" style="display: ruby-text-group; font-size: .70em; width: 80%; height: 15px; margin-top: 0.85em; margin-bottom: .67em; color: white; margin-left: 0; margin-right: 0;">poonch</h5>
 
                         <div style="height: 20px"></div>
                     </li>
